5819774dc01cc63f681e2e8b4458e359d4879bbd
906 <906@SHACHAM6>
2017-05-29 16:19:07 +0300

    Add project files.

---
+++ UWPProject/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UWPProject.Pages;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            mainFrame.Navigate(typeof(HomePage));
+        }
+
+        private void listBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            ListBox lb = sender as ListBox;
+
+            switch (lb.SelectedIndex)
+            {
+                case 0:
+                    mainFrame.Navigate(typeof(HomePage));
+                    break;
+                case 1:
+                    mainFrame.Navigate(typeof(ThemesPage));
+                    break;
+            }
+        }
+    }
+}
---
+++ UWPProject/Models/Photo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace UWPProject.Models
+{
+    public class Photo
+    {
+        public int ID { get; set; }
+        public string Path { get; set; }
+        public string Comments { get; set; }
+        public DateTime? Time { get; set; }
+        public Geolocator Location { get; set; }
+    }
+}
